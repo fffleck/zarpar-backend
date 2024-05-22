754661b7289085ae2f85a1179444a1f619bc4593
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-05-22 15:10:05 -0300

    ajustando bots

--- dist/controllers/cotacao_services/fretes.js
+++ dist/controllers/cotacao_services/fretes.js
@@ -26,7 +26,7 @@ const fretes = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     response_freight = yield adicionar_servico(response_freight, req, res, searatesController_1.searates);
     // response_freight = yield adicionar_servico(response_freight, req, res, zimController_1.zim);
     response_freight = yield adicionar_servico(response_freight, req, res, maerskController_1.maersk);
-    response_freight = yield adicionar_servico(response_freight, req, res, evergreenController_1.evergreen);
+    // response_freight = yield adicionar_servico(response_freight, req, res, evergreenController_1.evergreen);
     response_freight = yield adicionar_servico(response_freight, req, res, cmaController_1.cma);
     response_freight = yield adicionar_servico(response_freight, req, res, localController_1.local);
 
