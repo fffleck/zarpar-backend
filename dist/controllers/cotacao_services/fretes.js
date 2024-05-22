48dcf10a562303634fe0157e2149e3f90783ab0b
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-05-22 14:59:40 -0300

    ajustando bots

--- dist/controllers/cotacao_services/fretes.js
+++ dist/controllers/cotacao_services/fretes.js
@@ -23,9 +23,9 @@ const fretes = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.setHeader("Access-Control-Allow-Headers", "*");
     let response_freight;
     response_freight = [];
-    // response_freight = yield adicionar_servico(response_freight, req, res, searatesController_1.searates);
-    response_freight = yield adicionar_servico(response_freight, req, res, zimController_1.zim);
-    // response_freight = yield adicionar_servico(response_freight, req, res, maerskController_1.maersk);
+    response_freight = yield adicionar_servico(response_freight, req, res, searatesController_1.searates);
+    // response_freight = yield adicionar_servico(response_freight, req, res, zimController_1.zim);
+    response_freight = yield adicionar_servico(response_freight, req, res, maerskController_1.maersk);
     response_freight = yield adicionar_servico(response_freight, req, res, evergreenController_1.evergreen);
     response_freight = yield adicionar_servico(response_freight, req, res, cmaController_1.cma);
     response_freight = yield adicionar_servico(response_freight, req, res, localController_1.local);
