8d4bba572cf2df9d0a4dfd2122bb2013138fd87b
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-07-01 20:37:09 -0300

    Atualizando url bots˜

--- src/controllers/cotacao_services/zimController.ts
+++ src/controllers/cotacao_services/zimController.ts
@@ -52,10 +52,10 @@ export const zim = async (req: Request, res: Response) => {
     // console.log("URL ZIM ", `https://karavel-services-e63c55605b2e.herokuapp.com/zim?data_saida=${data_saida_zim}&porto_embarque=${porto_embarque}&porto_descarga=${porto_descarga}&tipo_container=${tipo_container}`);
 
     try {
-      let api_zim_res = await axios.get(`http://localhost:5001/zim?data_saida=${data_saida_zim}&porto_embarque=${porto_embarque}&porto_descarga=${porto_descarga}&tipo_container=${tipo_container}`)
-      // let api_zim_res = await axios.get(
-      //   `https://karavel-services-e63c55605b2e.herokuapp.com/zim?data_saida=${data_saida_zim}&porto_embarque=${porto_embarque}&porto_descarga=${porto_descarga}&tipo_container=${tipo_container}`
-      // );
+      // let api_zim_res = await axios.get(`http://localhost:5001/zim?data_saida=${data_saida_zim}&porto_embarque=${porto_embarque}&porto_descarga=${porto_descarga}&tipo_container=${tipo_container}`)
+      let api_zim_res = await axios.get(
+        `https://karavel-services-e63c55605b2e.herokuapp.com/zim?data_saida=${data_saida_zim}&porto_embarque=${porto_embarque}&porto_descarga=${porto_descarga}&tipo_container=${tipo_container}`
+      );
 
       api_zim_res.data.forEach((result: any) => {
         response_freight.push(result);
