2c1991ed50016fb0e69f559a8c746e9e6fe4416b
Fabio Fleck <fabio.fleck.external@lojasrenner.com.br>
2024-09-04 17:22:17 -0300

    Mudando BD

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 export default function connectDatabase(){
   mongoose
     .connect(
-      // `mongodb+srv://karavel_app:karavelapp2023@cluster0.oem86y1.mongodb.net/karavel?retryWrites=true&w=majority`,
-      `mongodb+srv://karavel_app:karavelapp2023@cluster0.oem86y1.mongodb.net/karavel_dev?retryWrites=true&w=majority`,
+      `mongodb+srv://karavel_app:karavelapp2023@cluster0.oem86y1.mongodb.net/karavel?retryWrites=true&w=majority`,
+      // `mongodb+srv://karavel_app:karavelapp2023@cluster0.oem86y1.mongodb.net/karavel_dev?retryWrites=true&w=majority`,
       (err) => {
         if (err) {
           console.error('FAILED TO CONNECT TO MONGODB');
